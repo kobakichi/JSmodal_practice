26fec72dc2f6f813050a4a15a5ecd421175eeb0f
kobakichi <fenderstrat57vin@gmail.com>
2022-07-07 04:28:58 +0900

    コメントを追加

--- js/main.js
+++ js/main.js
@@ -6,16 +6,19 @@
   const modal = document.getElementById("modal");
   const mask = document.getElementById("mask");
 
+  // 詳細を見るをクリックしたらid=mask,modalのhiddenクラスを削除して、modal表示
   open.addEventListener("click", () => {
     modal.classList.remove("hidden");
     mask.classList.remove("hidden");
   });
 
+  // id=closeをクリックしたらid=modal,maskにhiddenクラスを付けてmodalを非表示
   close.addEventListener("click", () => {
     modal.classList.add("hidden");
     mask.classList.add("hidden");
   });
 
+  // id=maskにもcloseと同じ処理をする。
   mask.addEventListener("click", () => {
     close.click();
   });
